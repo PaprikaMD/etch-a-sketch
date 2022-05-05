03cc1a4b864a292b374f43377c153c068f9059da
Grant Deedler <grantdeedler@gmail.com>
2022-05-05 18:40:05 -0400

    Create functional etch-a-sketch grid

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 function main(){
-    generateGrid('#grid', 16)
+    generateGrid('#grid', 16);
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => square.style.backgroundColor = 'white')
+    })
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => prompt('Grid size'));
 
 }
 
@@ -13,11 +19,15 @@ function generateGrid(elementName, size){
             let innerElement = document.createElement('div');
             innerElement.setAttribute('class', 'row square');
             innerElement.setAttribute('index',i);
-            innerElement.textContent = '#';
             newElement.appendChild(innerElement);
         }
         element.appendChild(newElement);
     }
 }
+
+function resetGrid(size){
+
+}
+
 
 main();
